cadb37961c3e00fe759648cf30db915720cdf4ef
CSB Lab User Account <csb-lab@fresnostate.edu>
2017-03-30 15:35:10 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+links: Array<Object>;
+title: string;
+showLink: boolean;
+constructor(private appService:AppService){
+
+}
+ngOnInit(){
+
+this.title = 'hello you!';
+this.link = [
+  {text:'Ronnie', href:'http://google.com'},
+  {text:'lass', href:'http://google.com'},
+  {text:'Mary', href:'http://google.com'},
+  {text:'John', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'},
+  {text:'Link 1', href:'http://google.com'}
+]
+this.showLink = true;
+}
+
+toggleLink(){
+this.showLink = this.appService.toggle(this.showLink);
+console.log('this.showLink is ...', this.showLink);
 }
---
+++ src/app/app.servise.ts
@@ -0,0 +1,18 @@
+import {Component, Injectable} from '@angular/core';
+
+@Injectable()
+export class AppService{
+
+// this is where the variables go
+
+constructor() {
+//do init stuff
+}
+//this is where the function goes..
+
+toggleLink(prop:boolean){
+  let output = !prop;
+  console.log('from app.service toggle method....', output);
+  return output;
+}
+}
